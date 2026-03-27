bde518c40cea4e340f1f0cbcb79f3b0f9f4ff3af
Lucas Cloes <lucas.cloes@gmail.com>
2026-03-27 13:55:37 +0100

    debug upload s3

--- main.go
+++ main.go
@@ -1,57 +1,73 @@
 package main
 
 import (
-    "context"
-    "database/sql"
-    "log"
-    "net/http"
-    "os"
+	"context"
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
 
-    "github.com/aws/aws-sdk-go-v2/aws"
-    "github.com/aws/aws-sdk-go-v2/config"
-    "github.com/aws/aws-sdk-go-v2/service/s3"
-    "github.com/cccLUCASccc/Centra-API/routes"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/cccLUCASccc/Centra-API/routes"
 
-    _ "github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-    // 1. Connexion DB
-    dsn := os.Getenv("DATABASE_URL")
-    db, err := sql.Open("postgres", dsn)
-    if err != nil {
-        log.Fatal("Erreur connexion DB:", err)
-    }
+	// 1. Connexion à la Base de Données
+	dsn := os.Getenv("DATABASE_URL")
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal("Erreur ouverture DB:", err)
+	}
 
-    // 2. Configuration S3
+	if err := db.Ping(); err != nil {
+		log.Fatal("Impossible de joindre la DB:", err)
+	}
+
+	// 2. Lire les variables S3
+	bucketName := os.Getenv("AWS_S3_BUCKET_NAME")
+	endpointURL := os.Getenv("AWS_ENDPOINT_URL")
+	region := os.Getenv("AWS_DEFAULT_REGION")
+
+	log.Printf("DÉMARRAGE - Bucket: %s | Endpoint: %s", bucketName, endpointURL)
+
+	if bucketName == "" || endpointURL == "" {
+		log.Fatal("ERREUR: Variables S3 (Bucket ou Endpoint) manquantes dans Railway !")
+	}
+
+	// 3. Créer la config AWS
 	cfg, err := config.LoadDefaultConfig(context.TODO(),
-		config.WithRegion(os.Getenv("AWS_DEFAULT_REGION")),
+		config.WithRegion(region),
 	)
 	if err != nil {
 		log.Fatal("Erreur config AWS:", err)
 	}
 
+	// 4. Créer le client S3
 	s3Client := s3.NewFromConfig(cfg, func(o *s3.Options) {
-		o.BaseEndpoint = aws.String(os.Getenv("AWS_ENDPOINT_URL"))
-		o.UsePathStyle = true 
+		o.BaseEndpoint = aws.String(endpointURL)
+		o.UsePathStyle = true
 	})
-    
-    // 3. Initialisation de l'environnement des routes
-    api := &routes.Env{
-        DB:       db,
-        S3Client: s3Client,
-        Bucket:   os.Getenv("AWS_S3_BUCKET_NAME"),
-    }
 
-    // Routes
-    http.HandleFunc("/api/vehicules", api.ListeVehicules)
+	// 5. Passer les valeurs à l'environnement des routes
+	api := &routes.Env{
+		DB:       db,
+		S3Client: s3Client,
+		Bucket:   bucketName,
+	}
+
+	// Routes
+	http.HandleFunc("/api/vehicules", api.ListeVehicules)
 	http.HandleFunc("/api/vehicules/add", api.AjouterVehicule)
-   	
+
 	// Lancement
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "8080"
-    }
-    log.Printf("Serveur démarré sur le port %s", port)
-    log.Fatal(http.ListenAndServe(":"+port, nil))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Printf("Serveur démarré sur le port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
