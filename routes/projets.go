9ac4ff2036787dc457d91b8930da056950d759d7
Cloes Lucas <cloeslucas@MacBookAir.lan>
2026-04-03 06:27:48 +0200

    patch endoint

--- routes/projets.go
+++ routes/projets.go
@@ -1,16 +1,5 @@
 package routes
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"time"
-
-	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/lib/pq"
-)
-
 type Projet struct {
     ID          int      `json:"id"`
     Name        string   `json:"name"`
@@ -19,13 +8,28 @@ type Projet struct {
     Status      string   `json:"status"`
 }
 
-// 1. LISTER LES projets
+package routes
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/lib/pq"
+)
+
+// 1. LISTER LES projets (Corrigé pour correspondre à la logique véhicules)
 func (e *Env) ListeProjets(w http.ResponseWriter, r *http.Request) {
 	query := `
         SELECT p.id, p.name, p.description, p.status, 
                COALESCE(array_agg(pi.url) FILTER (WHERE pi.url IS NOT NULL), '{}')
         FROM projets p
-        LEFT JOIN projets_images pi ON p.id = pi.projet_id
+        LEFT JOIN projet_images pi ON p.id = pi.projet_id
         GROUP BY p.id`
 
 	rows, err := e.DB.Query(query)
@@ -36,11 +40,9 @@ func (e *Env) ListeProjets(w http.ResponseWriter, r *http.Request) {
 	}
 	defer rows.Close()
 
-	listeDeProjets := []Projet{}
-
+	var listeDeProjets []Projet
 	for rows.Next() {
 		var p Projet
-		// Utilise pq.Array pour le tableau d'images
 		err := rows.Scan(&p.ID, &p.Name, &p.Description, &p.Status, pq.Array(&p.Images))
 		if err != nil {
 			log.Printf("Erreur Scan: %v", err)
@@ -51,6 +53,58 @@ func (e *Env) ListeProjets(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(listeDeProjets)
+}
+
+// 2. AJOUTER UN Projet (Copié de la logique AjouterVehicule qui fonctionne)
+func (e *Env) AjouterProjet(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(10 << 20)
+
+	name := r.FormValue("name")
+	desc := r.FormValue("description")
+	status := r.FormValue("status")
+
+	var projetID int
+	queryProjet := `INSERT INTO projets (name, description, status) 
+                    VALUES ($1, $2, $3) RETURNING id`
+
+	err := e.DB.QueryRow(queryProjet, name, desc, status).Scan(&projetID)
+	if err != nil {
+		log.Printf("Erreur SQL Projet: %v", err)
+		http.Error(w, "Erreur insertion projet", 500)
+		return
+	}
+
+	// On utilise la même logique de boucle que pour les véhicules
+	files := r.MultipartForm.File["image"]
+	endpoint := os.Getenv("AWS_ENDPOINT_URL")
+
+	for _, header := range files {
+		file, err := header.Open()
+		if err != nil {
+			continue
+		}
+
+		fileName := fmt.Sprintf("%d-%s", time.Now().Unix(), header.Filename)
+
+		_, err = e.S3Client.PutObject(r.Context(), &s3.PutObjectInput{
+			Bucket: &e.Bucket,
+			Key:    &fileName,
+			Body:   file,
+			ACL:    "public-read", // On remet l'ACL qui fonctionne sur les véhicules
+		})
+		file.Close()
+
+		if err == nil {
+			imageURL := fmt.Sprintf("%s/%s/%s", endpoint, e.Bucket, fileName)
+			_, err = e.DB.Exec("INSERT INTO projet_images (projet_id, url) VALUES ($1, $2)", projetID, imageURL)
+			if err != nil {
+				log.Printf("Erreur SQL Image: %v", err)
+			}
+		}
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Projet ajouté !"})
 }
 
 // 2. AJOUTER UN Projet
